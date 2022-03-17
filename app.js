b9870a91199067c50cdfb2fc8c03c582de9b0c31
Rashid Riaz <rashidriax0@gmail.com>
2022-03-18 04:09:21 +0500

    bug fix, routing issue resolved

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 3000;
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile("./index.html", {root: __dirname});
 });
 
