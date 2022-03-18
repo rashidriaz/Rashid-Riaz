4958f54f501ee7dd7bb00bb9908f48a49561bc23
Rashid Riaz <rashidriax0@gmail.com>
2022-03-18 20:23:16 +0500

    adds contact screen

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require("express");
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3256;
 
 app.get("/", (req, res) => {
     res.sendFile("./index.html", {root: __dirname});
